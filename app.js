5a0629f5d12e424c3a94326948437a77092a4906
Maayan Meir <43983815+DevOpsNtmm@users.noreply.github.com>
2024-05-10 14:07:20 +0300

    Login register (#8)
    * configured routes
    
    * configured routes
    
    * changed signup to register, deleted irrelevant data
    
    * added register html file
    
    ---------
    
    Co-authored-by: yuval-benjamin <70840131+yuval-benjamin@users.noreply.github.com>

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ const mongoose = require('mongoose')
 const workoutsController = require('./controllers/workouts')
 const customersController = require('./controllers/customers')
 const server = express()
+const login = require('./routes/login')
+const workouts = require('./routes/workouts')
 
 server.use(express.static('public'))
-
-server.get("/home", workoutsController.GetHomePage)
-server.get("/nearme", workoutsController.GetNearMePage)
-
 server.set("view engine", "ejs");
-server.use("/", require("./routes/login"));
-server.use("/signup", require("./routes/login"))
-server.use(express.urlencoded({ extended: false }))
 
+server.use('/login', login)
+server.use("/", workouts);
+
+server.use(express.urlencoded({ extended: false }))
 
 mongoose.connect(process.env.MONGO_URL, { 
     useUnifiedTopology: true, 
