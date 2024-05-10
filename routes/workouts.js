ffb62198f3b71d076eb9aafb235d9992386f2417
yuval-benjamin <70840131+yuval-benjamin@users.noreply.github.com>
2024-05-10 19:14:00 +0300

    Mongo connection - GetWorkoutIfContains (#9)
    Adding the GetWorkoutIfContains

--- routes/workouts.js
+++ routes/workouts.js
@@ -6,4 +6,9 @@ const workoutsController  = require("../controllers/workouts");
 router.get("/", workoutsController.GetHomePage)
 router.get("/nearme", workoutsController.GetNearMePage)
 
+router.route('/:id')
+    .get(workoutsController.GetWorkout)
+    // .patch(workoutsController.updateWorkout)
+    // .delete(workoutsController.deleteWorkout);
+
 module.exports = router;
