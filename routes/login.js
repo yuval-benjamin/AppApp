9ea53b4f5bb718e7041601ec23fb22ed30ddd762
Maayan <maayan.meir99@gmail.com>
2024-05-10 13:33:20 +0300

    configured routes

--- routes/login.js
+++ routes/login.js
@@ -6,11 +6,8 @@ const loginController  = require("../controllers/login");
 router.get("/", loginController.loginForm);
 router.get("/signup", loginController.registerForm);
 // router.post("/register", loginController.register);
-router.get("/login", loginController.loginForm);
-router.get("/test", loginController.loginForm);
 // router.post("/login", loginController.login);
 // router.get('/logout',loginController.logout);
 // router.get('/', loginController.isLoggedIn, loginController.foo);
-
 
 module.exports = router;
