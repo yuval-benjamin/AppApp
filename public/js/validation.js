05765cbd782a9ab3543522b6880258b8452ab7dd
yuval-benjamin <jjuunnkkmm@gmail.com>
2024-07-27 08:51:22 +0300

    Update works but not validation

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,31 +1,23 @@
 $(document).ready(function() {
 
-    function validateFirstName() {
-        const firstName = $('#registerForm input[name="firstName"]').val().trim();
+    function validateFirstName(formId) {
+        const firstName = $(`${formId} input[name="firstName"]`).val().trim();
         if (firstName === '') {
             return false;
         }
         return true;
     }
 
-    function validateLastName() {
-        const lastName = $('#registerForm input[name="lastName"]').val().trim();
+    function validateLastName(formId) {
+        const lastName = $(`${formId} input[name="lastName"]`).val().trim();
         if (lastName === '') {
             return false;
         }
         return true;
     }
 
-    function validateUsername() {
-        const username = $('#registerForm input[name="username"]').val().trim();
-        if (username === '') {
-            return false;
-        }
-        return true;
-    }
-
-    function validateEmail() {
-        const email = $('#registerForm input[name="email"]').val().trim();
+    function validateEmail(formId) {
+        const email = $(`${formId} input[name="email"]`).val().trim();
         if (email === '') {
             return false;
         }
@@ -34,23 +26,23 @@ $(document).ready(function() {
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
         if (!emailPattern.test(email)) {
-            $('#emailError').text('Enter a valid email address');
+            $(`${formId} #emailError`).text('Enter a valid email address');
             return false;
         }
-        $('#emailError').text('');
+        $(`${formId} #emailError`).text('');
         return true;
     }
 
-    function validatePassword() {
-        const password = $('#registerForm input[name="password"]').val().trim();
+    function validatePassword(formId) {
+        const password = $(`${formId} input[name="password"]`).val().trim();
         if (password === '') {
             return false;
         }
         return true;
     }
 
-    function validateBirthDate() {
-        const userBirthDateString = $('#registerForm input[name="birthDate"]').val().trim();
+    function validateBirthDate(formId) {
+        const userBirthDateString = $(`${formId} input[name="birthDate"]`).val().trim();
         const userBirthDate = new Date(userBirthDateString); // Convert birthdate string to Date object
         const today = new Date();
         let age = today.getFullYear() - userBirthDate.getFullYear();
@@ -62,21 +54,29 @@ $(document).ready(function() {
         }
         
         if (age < 18) {
-            $('#birthDateError').text('Sorry, only 18 years old and up');
-            return false
+            $(`${formId} #birthDateError`).text('Sorry, only 18 years old and up');
+            return false;
         }
 
-        $('#birthDateError').text('');
-        return true
-
+        $(`${formId} #birthDateError`).text('');
+        return true;
     }
 
-    // Check on submit
-    $('#registerForm').submit(function(event) {
+    function validateForm(formId) {
+        return validateFirstName(formId) && validateLastName(formId) &&
+            validateEmail(formId) && validateBirthDate(formId) && validatePassword(formId);
+    }
 
-        // Prevent submit if not everything is ok
-        if (!validateFirstName() || !validateLastName() || !validateUsername() ||
-            !validateEmail() || !validateBirthDate() || !validatePassword()) {
+    // Check on submit of a registration form
+    $('#registerForm').submit(function(event) {
+        if (!validateForm('#registerForm')) {
+            event.preventDefault();
+        }
+    });
+
+    // Check on submit of an update form
+    $('#update-customer-form').submit(function(event) {
+        if (!validateForm('#update-customer-form')) {
             event.preventDefault();
         }
     });
