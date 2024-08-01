0ce1fb16551619e4ab55c53fa2eee78caa32ab89
Gili Yaacobi <giliyaacobi@Gilis-MBP.lan>
2024-08-01 18:09:30 +0300

    wip: gili

---
+++ public/js/navbar.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetch('/is-admin')
+      .then(response => response.json())
+      .then(data => {
+        if (data.isAdmin) {
+          document.getElementById('admin-button').style.display = 'block';
+        }
+      })
+      .catch(error => console.error('Error fetching admin status:', error));
+  });
+
+  async function redirectToCart() {
+    try {
+        const response = await fetch('/getSessionUsername');
+        const data = await response.json();
+        const username = data.username;
+
+        window.location.href = `http://localhost/cart/${username}`;
+    } catch (error) {
+        console.error('Error fetching username:', error);
+    }
+}
+
+// Cart listener
+document.getElementById('cart-link').addEventListener('click', function(event) {
+  event.preventDefault(); 
+  redirectToCart();
+});
--- public/js/workouts.js
+++ public/js/workouts.js
@@ -1,4 +1,3 @@
-
 // Fetch and display all workouts
 async function getAll() {
     const res = await fetch('http://localhost/workouts')
@@ -11,6 +10,8 @@ async function getAll() {
 async function getWorkouts(event) {
     event.preventDefault();
     const searchData = document.getElementById('search-input').value;
+
+    uncheckCheckboxes()
 
     if (!searchData){
         await fetchSelectedWorkouts(); 
@@ -57,7 +58,7 @@ async function fetchSelectedWorkouts() {
     else {
         const response = await fetch(`/workouts/selectedWorkouts?${queryString}`);
         const workoutsJs = await response.json();
-        
+
         displayWorkouts(workoutsJs)
     }
 }
@@ -84,7 +85,17 @@ function displayWorkouts(workoutsJs) {
     document.getElementsByClassName('workouts')[0].innerHTML = emptyWorkouts
     
     for (let i = 0; i < workoutsJs.length; i++) {
-      const element = workoutsJs[i];
+        const element = workoutsJs[i];
+        
+        if (typeof element.time === 'string' && !isNaN(Date.parse(element.time))) {
+            const date = new Date(element.time);
+            const day = date.getDate();
+            const month = (date.getMonth() + 1).toString().padStart(2, '0');
+            const year = date.getFullYear();
+            const hours = date.getHours();
+            const minutes = date.getMinutes().toString().padStart(2, '0');
+            element.time = `${hours}:${minutes} ${day}/${month}/${year}`;
+        }
       
       let workout = document.getElementById('workoutTemplate').innerHTML
       for (const key in element) {
@@ -95,6 +106,36 @@ function displayWorkouts(workoutsJs) {
     }
 }
 
+async function buyWorkout(event) {
+    const workoutId = event.target.closest('.col').getAttribute('data-id');
+    try {
+        const addToCartResponse = await fetch('/cart/addWorkoutToCart', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ workoutId: workoutId })
+        });
+        if (addToCartResponse.ok) {
+            console.log('Workout added to cart successfully');
+        } else {
+            console.error('Failed to add workout to cart');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+}
+
+function uncheckCheckboxes() {
+    // Get all checkboxes within the dropdown
+    var checkboxes = document.querySelectorAll('.dropdown input[type="checkbox"]');
+
+    // Loop through each checkbox and uncheck it
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = false;
+    });
+}
 
 
 // Initialize by fetching all workouts
@@ -105,7 +146,8 @@ const emptyWorkouts = document.getElementsByClassName('workouts')[0].innerHTML
 
 // Search button listener
 const submitButton = document.querySelector('#search-button');
-submitButton.addEventListener('click', getWorkouts);    
+submitButton.addEventListener('click', getWorkouts);
+
 
 // Checkbox listener
 document.querySelectorAll('.sub-options input[type="checkbox"]').forEach(function(checkbox) {
@@ -113,3 +155,12 @@ document.querySelectorAll('.sub-options input[type="checkbox"]').forEach(functio
         await fetchSelectedWorkouts(); 
     });
 });
+
+// Buy button listener
+document.querySelector('.workouts').addEventListener('click', function(event) {
+    if (event.target && event.target.matches('.buy-button')) {
+        buyWorkout(event);
+    }
+});
+
+
